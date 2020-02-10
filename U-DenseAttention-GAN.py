474217c3874edbfeca4c30dfbac2f43d1cc245a6
Razi <razi.rezaei@tum.de>
2020-02-10 16:31:29 +0100

    Update U-DenseAttention-GAN.py

--- U-DenseAttention-GAN.py
+++ U-DenseAttention-GAN.py
@@ -1,22 +1,55 @@
 Img
 
+1)
 Cn,BN,ReLU
 Cn
 [skp cntion1]
-MaxP
+MaxP_from_DenseBlock
+
+2)
 Cn,BN,ReLU
 Cn
 [skp cntion2]
-MaxP
+MaxP_from_DenseBlock
+
+3)
 Cn,BN,ReLU
 Cn
 [skp cntion3]
-MaxP
+MaxP_from_DenseBlock
+
+4)
 Cn,BN,ReLU
 Cn
 [skp cntion4]
-MaxP
-Cn
+MaxP_from_DenseBlock
+
+5)
+Cn,BN,ReLU
+
+6)
+gating_Signal4
 UpSmp
+Cn,BN,ReLU
+Cn
+
+7)
+gating_Signal3
+UpSmp
+Cn,BN,ReLU
+Cn
+
+8)
+gating_Signal2
+UpSmp
+Cn,BN,ReLU
+Cn
+
+9)
+gating_Signal1
+UpSmp
+Cn,BN,ReLU
+Cn
+
 
 
